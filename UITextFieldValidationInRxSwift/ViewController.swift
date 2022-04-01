62204ccfee345f680a9767d74fa24e3a16542319
Wataru Miyakoshi <wtrmiya@gmail.com>
2022-04-02 05:23:13 +0900

    Initial Commit

---
+++ UITextFieldValidationInRxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextFieldValidationInRxSwift
+//
+//  Created by Wataru Miyakoshi on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
